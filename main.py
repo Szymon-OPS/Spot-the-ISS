e9a4fd7cf294c461b5971ef36b65e875cb2f5564
Szymon Szczepiński <75580477+Szymon-OPS@users.noreply.github.com>
2021-09-24 00:07:30 +0200

    Update main.py
    -added email notification

--- main.py
+++ main.py
@@ -1,12 +1,18 @@
 #------------------- IMPORTS AND CONSTANTS --------------------------------------------
 
 import requests
+import time
+import smtplib
 from datetime import datetime
 
 # Add lat and lon of desired observation location from which you intend
 # to observe the ISS:
 MY_LAT = 53.734089
 MY_LON = 18.926979
+
+# Email data - where notifications will be sent
+MY_EMAIL = "Your email"
+MY_PASSWORD = "Your password"
 
 #------------------- ISS POSITION -----------------------------------------------------
 
@@ -65,3 +71,23 @@ def is_night():
     
     if current_hour < sunrise or current_hour > sunset:
         return True
+
+#------------------- EMAIL NOTIFICATION -----------------------------------------------
+
+while True:
+    # Iteration frequency
+    time.sleep(60)
+    if is_iss_overhead() and is_night():
+        
+        # Use smtplib to connect to your email (Check port!)
+        with smtplib.SMTP("smtp.mail.yahoo.com", 587) as connection:
+            # Start TLS(transport layer security) = secure connection
+            connection.starttls()
+            # Login to account
+            connection.login(user=MY_EMAIL, password=MY_PASSWORD)
+            # Send email
+            connection.sendmail(
+                from_addr=MY_EMAIL,
+                to_addrs=MY_EMAIL,
+                msg=f"Subject:ISS Overhead!\n\nLook up! It's dark and ISS is somewhere over you!"
+            )
