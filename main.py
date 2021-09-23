8545b21cb34952b167f061a6af6e462cc56b383e
Szymon Szczepiński <75580477+Szymon-OPS@users.noreply.github.com>
2021-09-23 22:56:24 +0200

    Update main.py
    -added is_night() to ensure visibility in my location
    -imported datetime

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 #------------------- IMPORTS AND CONSTANTS --------------------------------------------
 
 import requests
+from datetime import datetime
 
 # Add lat and lon of desired observation location from which you intend
 # to observe the ISS:
@@ -26,4 +27,41 @@ def is_iss_overhead():
     
     # Check if ISS is +-5 degrees from your position (ensure visibility)
     if MY_LAT-5 <= iss_latitude <= MY_LAT+5 and MY_LON-5 <= iss_longitude <= MY_LON+5:
+        return True
+
+#------------------- VISIBILITY IN MY POSITION ----------------------------------------
+
+def is_night():
+    # Store input parameters - same NAMES as in API documentation!
+    # "formatted" specifies 12/24h format
+    parameters = {
+        "lat": MY_LAT,
+        "lng": MY_LON,
+        "formatted": 0
+    }
+
+    response = requests.get(url="https://api.sunrise-sunset.org/json",
+                            params=parameters)
+    
+    # Exact exception will be raised if unsuccessful
+    response.raise_for_status()
+
+    # Get hold of response data
+    data = response.json()
+    
+    # Get hold of sunset/sunrise hour in 24h format
+    sunrise = int(data['results']['sunrise'].split("T")[1].split(":")[0])
+    sunset = int(data['results']['sunset'].split("T")[1].split(":")[0])
+    
+    # Test
+    # print(sunrise)
+    # print(sunset)
+
+    time_now = datetime.now()
+    current_hour = int(time_now.hour)
+    
+    # Test
+    # print(time_now.hour)
+    
+    if current_hour < sunrise or current_hour > sunset:
         return True
